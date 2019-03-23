5f1f13a513d47eb2d9b03cf9aba1b5cc69ace32d
Eric Dulcet <etdulcet@gmail.com>
2019-03-23 10:32:14 -0400

    Create basic svg dials

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import React, { useState } from "react";
 import "./App.css";
+import _ from "lodash";
+
+const components = [
+  {
+    uuid: Math.random(),
+    type: "Dial",
+    cx: 25,
+    cy: 25,
+    r: 10
+  },
+  {
+    uuid: Math.random(),
+    type: "Dial",
+    cx: 50,
+    cy: 25,
+    r: 10
+  }
+];
+
+function Dials() {
+  return (
+    <>
+      {components.map(component => {
+        return (
+          <circle
+            key={component.uuid}
+            cx={component.cx}
+            cy={component.cy}
+            r={component.r}
+            stroke="black"
+            strokeWidth="1"
+            fill="none"
+          />
+        );
+      })}
+    </>
+  );
+}
 
 function App() {
   const [height, setHeight] = useState(200);
@@ -12,11 +50,12 @@ function App() {
           width={width}
           height={height}
           style={{
-            fill: "rgb(100,100,100)",
+            fill: "none",
             strokeWidth: 2,
             stroke: "rgb(0,0,0)"
           }}
         />
+        <Dials />
       </svg>
       <div className="dimensions">
         <input
