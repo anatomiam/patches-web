a323e9706ce5380d2f99738227a64afdde2a399e
Eric Dulcet <etdulcet@gmail.com>
2019-04-02 20:16:15 -0400

    Set up Knobs component to query knobs data from db

--- src/components/Knobs.js
+++ src/components/Knobs.js
@@ -3,39 +3,69 @@ import { FootSwitch } from "./FootSwitch";
 import { Knob } from "./Knob";
 import { Switch } from "./Switch";
 import { useStateValue } from "../StateProvider";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+  query {
+    knobs {
+      id
+      type
+      description
+      cx
+      cy
+      r
+      angle
+      pedal {
+        id
+      }
+      builder {
+        id
+      }
+    }
+  }
+`;
 
 export const Knobs = () => {
-  const [{ pedal }, dispatch] = useStateValue();
+  const [, dispatch] = useStateValue();
 
-  const { knobs } = pedal.components;
   return (
-    <>
-      {knobs.map(knob => {
-        switch (knob.type) {
-          case "FootSwitch":
-            return (
-              <FootSwitch
-                key={knob.uuid}
-                footSwitchDetails={knob}
-                dispatch={dispatch}
-              />
-            );
-          case "Knob":
-            return (
-              <Knob key={knob.uuid} knobDetails={knob} dispatch={dispatch} />
-            );
-          case "Switch":
-            return (
-              <Switch
-                key={knob.uuid}
-                switchDetails={knob}
-                dispatch={dispatch}
-              />
-            );
-          default:
-            return null;
-        }
-      })}
-    </>
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) return <div>Error</div>;
+
+        const { knobs } = data;
+        console.log(data);
+
+        return knobs.map(knob => {
+          switch (knob.type) {
+            case "FootSwitch":
+              return (
+                <FootSwitch
+                  key={knob.id}
+                  footSwitchDetails={knob}
+                  dispatch={dispatch}
+                />
+              );
+            case "Knob":
+              console.log(knob);
+              return (
+                <Knob key={knob.id} knobDetails={knob} dispatch={dispatch} />
+              );
+            case "Switch":
+              return (
+                <Switch
+                  key={knob.id}
+                  switchDetails={knob}
+                  dispatch={dispatch}
+                />
+              );
+            default:
+              return null;
+          }
+        });
+      }}
+    </Query>
   );
 };
