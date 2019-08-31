765590aad4340ac9d8b05029ca5923af0a149383
Eric Dulcet <etdulcet@gmail.com>
2019-08-31 10:01:01 -0400

    Move the Builder styling into the component

--- src/components/Pages/Builder.js
+++ src/components/Pages/Builder.js
@@ -5,10 +5,45 @@ import { AvailablePedals } from "../Forms/AvailablePedals";
 import { ComponentInfo } from "../ComponentInfo";
 import { Pedal } from "../Parts/Pedal";
 import { PedalForm } from "../Forms/PedalForm";
-import "../../index.css";
 import { useStateValue } from "../../StateProvider";
 import { CreatePedalButton } from "../Forms/CreatePedalButton";
 import { Segment } from "semantic-ui-react";
+
+const styles = {
+  builderContainer: {
+    display: "grid",
+    gridTemplateRows: "100px 100px repeat(5, minmax(75px, 125px)) 75px 75px",
+    gridTemplateColumns: "repeat(9, 1fr)",
+    gridGap: "1px"
+  },
+  builderPedal: {
+    gridColumn: "3 / 8",
+    gridRow: "2 / 8",
+    placeSelf: "start center"
+  },
+  builderForm: {
+    gridColumn: "1 / 3",
+    gridRow: "1 / 8"
+  },
+  builderDetails: {
+    gridColumn: "8 / 10",
+    gridRow: "3 / 7"
+  },
+  builderPedalDetails: {
+    gridColumn: "8 / 10",
+    gridRow: "2 / 3"
+  },
+  builderPedalSelector: {
+    gridColumn: "8 / 10",
+    gridRow: "1 / 2",
+    placeSelf: "center"
+  },
+  builderSubmit: {
+    gridColumn: "8 / 10",
+    gridRow: "7 / 8",
+    placeSelf: "center end"
+  }
+};
 
 const Builder = ({ pedals }) => {
   // const { knobs, width, height, name } = pedal;
@@ -19,8 +54,8 @@ const Builder = ({ pedals }) => {
 
   return (
     <>
-      <div className="builder-container">
-        <div className="builder-form">
+      <div style={styles.builderContainer}>
+        <div style={styles.builderForm}>
           <Segment color="red">
             <h2>{name}</h2>
             <PedalForm
@@ -37,7 +72,7 @@ const Builder = ({ pedals }) => {
         <Segment color="teal">
           <AddSwitchForm dispatch={dispatch} />
         </Segment> */}
-        <div className="builder-pedal">
+        <div style={styles.builderPedal}>
           <Pedal
             knobs={knobs}
             width={width}
@@ -46,19 +81,19 @@ const Builder = ({ pedals }) => {
             dispatch={dispatch}
           />
         </div>
-        <div className="builder-pedal-selector">
+        <div style={styles.builderPedalSelector}>
           <Segment color="orange">
             <AvailablePedals pedals={pedals} dispatch={dispatch} />
           </Segment>
         </div>
-        <div className="builder-pedal-details">
+        <div style={styles.builderPedalDetails}>
           <Segment color="black">
             <div>pedal width</div>
             <div>pedal height</div>
             <div>name or somthing</div>
           </Segment>
         </div>
-        <div className="builder-details">
+        <div style={styles.builderDetails}>
           <Segment color="black">
             <ComponentInfo
               knobs={knobs}
@@ -68,7 +103,7 @@ const Builder = ({ pedals }) => {
             />
           </Segment>
         </div>
-        <div className="builder-submit">
+        <div style={styles.builderSubmit}>
           <Segment color="olive">
             <CreatePedalButton localState={localState} />
           </Segment>
