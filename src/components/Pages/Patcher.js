fb5ccac4b71ac17f412d9745b0b509ff26a40312
Eric Dulcet <etdulcet@gmail.com>
2019-08-31 10:16:33 -0400

    Set up css grid for Patcher page

--- src/components/Pages/Patcher.js
+++ src/components/Pages/Patcher.js
@@ -4,8 +4,34 @@ import { ComponentInfo } from "../ComponentInfo";
 import { Pedal } from "../Parts/Pedal";
 import "../../index.css";
 import { useStateValue } from "../../StateProvider";
-import { Form, Grid, TextArea } from "semantic-ui-react";
+import { Form, TextArea } from "semantic-ui-react";
 
+const styles = {
+  patcherContainer: {
+    display: "grid",
+    gridTemplateRows: "100px 100px repeat(5, minmax(75px, 125px)) 75px 75px",
+    gridTemplateColumns: "repeat(9, 1fr)",
+    gridGap: "1px"
+  },
+  patcherPedal: {
+    gridColumn: "3 / 8",
+    gridRow: "2 / 8",
+    placeSelf: "start center"
+  },
+  patcherDetails: {
+    gridColumn: "8 / 10",
+    gridRow: "3 / 7"
+  },
+  patcherPedalSelector: {
+    gridColumn: "8 / 10",
+    gridRow: "1 / 2",
+    placeSelf: "center"
+  },
+  patcherNotes: {
+    gridColumn: "1 / 3",
+    gridRow: "1 / 8"
+  }
+};
 const Patcher = ({ pedals }) => {
   // const { knobs, width, height, name } = pedal;
   const [{ localState }, dispatch] = useStateValue();
@@ -15,33 +41,33 @@ const Patcher = ({ pedals }) => {
 
   return (
     <>
-      <Grid>
-        <Grid.Row centered columns={3}>
-          <Grid.Column>
-            <Pedal
-              knobs={knobs}
-              width={width}
-              height={height}
-              name={name}
-              dispatch={dispatch}
-            />
-          </Grid.Column>
-          <Grid.Column>
-            <Form>
-              <TextArea placeholder="Tell us more" />
-            </Form>
-            <ComponentInfo
-              knobs={knobs}
-              dispatch={dispatch}
-              selectedComponentId={selectedComponentId}
-              selectedComponentAngle={selectedComponentAngle}
-            />
-          </Grid.Column>
-          <Grid.Column>
-            <AvailablePedals pedals={pedals} dispatch={dispatch} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <div style={styles.patcherContainer}>
+        <div style={styles.patcherPedal}>
+          <Pedal
+            knobs={knobs}
+            width={width}
+            height={height}
+            name={name}
+            dispatch={dispatch}
+          />
+        </div>
+        <div style={styles.patcherNotes}>
+          <Form>
+            <TextArea placeholder="Tell us more" />
+          </Form>
+        </div>
+        <div style={styles.patcherDetails}>
+          <ComponentInfo
+            knobs={knobs}
+            dispatch={dispatch}
+            selectedComponentId={selectedComponentId}
+            selectedComponentAngle={selectedComponentAngle}
+          />
+        </div>
+        <div style={styles.patcherPedalSelector}>
+          <AvailablePedals pedals={pedals} dispatch={dispatch} />
+        </div>
+      </div>
     </>
   );
 };
