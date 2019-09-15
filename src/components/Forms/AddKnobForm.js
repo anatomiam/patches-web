0628f998bcc90b6253e2ab401b7f624551ec4c29
Eric Dulcet <etdulcet@gmail.com>
2019-09-15 10:00:23 -0400

    Refactor AddKnobForm with Formik and Yup

--- src/components/Forms/AddKnobForm.js
+++ src/components/Forms/AddKnobForm.js
@@ -1,106 +1,140 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Input, Form } from "semantic-ui-react";
+import { ValidationErrors } from "./ValidationErrors";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { isEmpty } from "lodash";
+
+const AddKnobFormSchema = Yup.object().shape({
+  cx: Yup.number()
+    .min(0, "Center X cannot be a negative number")
+    .max(750, "Center X cannot be greater than 750 pixels")
+    .required("Center X is required"),
+  cy: Yup.number()
+    .min(0, "Center Y cannot be a negative number")
+    .max(750, "Center Y cannot be greater than 750 pixels")
+    .required("Center Y is required"),
+  r: Yup.number()
+    .min(1, "Radius must be at least 1")
+    .max(750, "r cannot be greater than 750 pixels")
+    .required("r is required"),
+  angle: Yup.string().required("Angle is required"),
+  description: Yup.string()
+    .min(2, "Description must be at least 2 characters long.")
+    .max(50, "Description must be less than 50 characters long")
+    .required("Description is required")
+});
 
 export const AddKnobForm = React.memo(({ saveKnob, dispatch }) => {
-  const [cx, setCx] = useState("");
-  const [cy, setCy] = useState("");
-  const [r, setR] = useState("");
-  const [angle, setAngle] = useState("");
-  const [description, setDescription] = useState("");
-  // TODO generate a real id
   const tempId = Math.random();
 
   return (
-    <Form
-      onSubmit={event => {
-        event.preventDefault();
+    <Formik
+      initialValues={{
+        cx: 0,
+        cy: 0,
+        r: 0,
+        angle: 0,
+        description: ""
+      }}
+      validationSchema={AddKnobFormSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+      onSubmit={(values, { setSubmitting }) => {
         dispatch({
           type: "ADD_KNOB",
           knob: {
             type: "Knob",
-            cx,
-            cy,
-            r,
-            angle,
-            description,
+            cx: values.cx,
+            cy: values.cy,
+            r: values.r,
+            angle: values.angle,
+            description: values.description,
             id: tempId
           }
         });
-
-        setCx("");
-        setCy("");
-        setR("");
-        setAngle("");
-        setDescription("");
+        setSubmitting(false);
       }}
     >
-      <Form.Field>
-        <Input
-          label="CX"
-          placeholder="Set cx"
-          name="set-cx"
-          type="number"
-          onChange={event => {
-            setCx(parseFloat(event.target.value));
+      {({
+        dirty,
+        values,
+        errors,
+        handleChange,
+        handleReset,
+        isSubmitting,
+        handleSubmit
+      }) => (
+        <Form
+          onSubmit={event => {
+            event.preventDefault();
+            handleSubmit();
           }}
-          value={cx}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Input
-          id="cy"
-          label="CY"
-          placeholder="Set cy"
-          name="set-cy"
-          type="number"
-          onChange={event => {
-            setCy(parseFloat(event.target.value));
-          }}
-          value={cy}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Input
-          id="r"
-          label="R"
-          placeholder="Set r"
-          name="set-r"
-          type="number"
-          onChange={event => {
-            setR(parseFloat(event.target.value));
-          }}
-          value={r}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Input
-          id="angle"
-          label="Angle"
-          placeholder="Set angle"
-          name="set-angle"
-          type="number"
-          onChange={event => {
-            setAngle(parseFloat(event.target.value));
-          }}
-          value={angle}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Input
-          id="description"
-          label="Desc."
-          placeholder="Set description"
-          name="set-description"
-          type="text"
-          onChange={event => {
-            setDescription(event.target.value);
-          }}
-          value={description}
-        />
-      </Form.Field>
-      <Button size="mini" type="submit">
-        Add Knob
-      </Button>
-    </Form>
+        >
+          <Form.Field>
+            <Input
+              id="cx"
+              label="CX"
+              placeholder="Set cx"
+              name="cx"
+              type="number"
+              onChange={handleChange}
+              value={values.cx}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="cy"
+              label="CY"
+              placeholder="Set cy"
+              name="cy"
+              type="number"
+              onChange={handleChange}
+              value={values.cy}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="r"
+              label="R"
+              placeholder="Set r"
+              name="r"
+              type="number"
+              onChange={handleChange}
+              value={values.r}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="angle"
+              label="Angle"
+              placeholder="Set angle"
+              name="angle"
+              type="number"
+              onChange={handleChange}
+              value={values.angle}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="description"
+              label="Desc."
+              placeholder="Set description"
+              name="description"
+              type="text"
+              onChange={handleChange}
+              value={values.description}
+            />
+          </Form.Field>
+          <Button type="submit" disabled={isSubmitting}>
+            Add Knob
+          </Button>
+          <Button type="button" disabled={!dirty} onClick={handleReset}>
+            Reset
+          </Button>
+          {!isEmpty(errors) ? <ValidationErrors errors={errors} /> : null}
+        </Form>
+      )}
+    </Formik>
   );
 });
