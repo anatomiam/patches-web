075eaf0be53185c993d8eeb2a56ca5edc1de4191
Eric Dulcet <etdulcet@gmail.com>
2019-09-22 15:38:35 -0400

    Convert UpdateCxButton to Formik form

--- src/components/Forms/UpdateCxButton.js
+++ src/components/Forms/UpdateCxButton.js
@@ -1,34 +1,65 @@
-import React, { useState } from "react";
-import { Button, Input } from "semantic-ui-react";
+import React from "react";
+import { Button, Input, Form } from "semantic-ui-react";
+import { ValidationErrors } from "./ValidationErrors";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { isEmpty } from "lodash";
+
+const UpdateCxButtonSchema = Yup.object().shape({
+  cx: Yup.number()
+    .min(0, "Center X cannot be a negative number")
+    .max(750, "Center X cannot be greater than 750 pixels")
+    .required("Center X is required")
+});
 
 export const UpdateCxButton = React.memo(({ knobId, cx, dispatch }) => {
-  const [updatedCx, setUpdatedCx] = useState(cx);
-
   return (
-    <Input
-      size="mini"
-      type="number"
-      placeholder="Update cx"
-      onChange={event => {
-        setUpdatedCx(event.target.value);
+    <Formik
+      enableReinitialize
+      initialValues={{ cx }}
+      validationSchema={UpdateCxButtonSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+      onSubmit={(values, { setSubmitting }) => {
+        dispatch({
+          type: "UPDATE_CX",
+          selectedComponentId: knobId,
+          cx: values.cx
+        });
+        setSubmitting(false);
       }}
-      value={updatedCx}
-      action={
-        <Button
-          icon="add"
-          size="mini"
-          className="icon-pointer"
-          onClick={event => {
+    >
+      {({ values, errors, handleChange, isSubmitting, handleSubmit }) => (
+        <Form
+          onSubmit={event => {
             event.preventDefault();
-            console.log(updatedCx);
-            dispatch({
-              type: "UPDATE_CX",
-              selectedComponentId: knobId,
-              cx: updatedCx
-            });
+            handleSubmit();
           }}
-        />
-      }
-    />
+        >
+          <Form.Field>
+            <Input
+              size="mini"
+              id="cx"
+              placeholder="Update cx"
+              name="cx"
+              type="number"
+              value={values.cx}
+              onChange={handleChange}
+              action={
+                <Button
+                  icon="resize horizontal"
+                  size="mini"
+                  color="blue"
+                  className="icon-pointer"
+                  type="submit"
+                  disabled={isSubmitting}
+                />
+              }
+            />
+          </Form.Field>
+          {!isEmpty(errors) ? <ValidationErrors errors={errors} /> : null}
+        </Form>
+      )}
+    </Formik>
   );
 });
