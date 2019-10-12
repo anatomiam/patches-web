a163c3e27b5bea9d37e4bbfe9aa2a289594448a0
Eric Dulcet <etdulcet@gmail.com>
2019-10-12 10:30:05 -0400

    Move all builder forms into new toolbar, remove Drag and Tap buttons

--- src/components/Modes/Builder/Builder.js
+++ src/components/Modes/Builder/Builder.js
@@ -9,24 +9,21 @@ import { Pedal } from "../../DeviceComponents/Body/Pedal";
 import { PedalForm } from "../../Forms/Builder/PedalForm";
 import { useStateValue } from "../../../state/StateProvider";
 import { CreatePedalButton } from "../../Forms/Builder/CreatePedalButton";
-import { DragKnobButton } from "../../Forms/Builder/DragKnobButton";
 import { UpdatePedalButton } from "../../Forms/Builder/UpdatePedalButton";
 import { StartFromScratchButton } from "../../Forms/Builder/StartFromScratchButton";
 import { Scaler } from "../General/Scaler";
-import { Accordion, Icon } from "semantic-ui-react";
+import { Icon, Menu, Popup } from "semantic-ui-react";
 import {
   DivContainer,
   DivDetails,
-  DivForm,
   DivPedal,
-  DivPedalSelector,
   DivSubmit,
   DivTools
 } from "../PageStyles";
-import { TapKnobsInButton } from "../../Forms/Builder/tapKnobsInButton";
 
 const Builder = ({ pedals }) => {
   const [{ localState }, dispatch] = useStateValue();
+  const [activeItem, setActiveItem] = useState("");
   const { width, height, name, color, id } = localState.pedalDetails;
   const {
     knobs,
@@ -37,13 +34,6 @@ const Builder = ({ pedals }) => {
     drag,
     tapKnobsIn
   } = localState;
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleClick = (e, titleProps) => {
-    const { index } = titleProps;
-    const newIndex = activeIndex === index ? -1 : index;
-    setActiveIndex(newIndex);
-  };
 
   return (
     <DivContainer>
@@ -60,24 +50,50 @@ const Builder = ({ pedals }) => {
         />
       </DivPedal>
       <DivTools>
-        <DivPedalSelector>
-          <AvailablePedals pedals={pedals} dispatch={dispatch} />
-        </DivPedalSelector>
-        <DragKnobButton drag={drag} dispatch={dispatch} />
-        <TapKnobsInButton tapKnobsIn={tapKnobsIn} dispatch={dispatch} />
-        <Scaler scale={scale} dispatch={dispatch} />
-        <DivForm>
-          <h2>{name}</h2>
-          <Accordion styled>
-            <Accordion.Title
-              active={activeIndex === 0}
-              index={0}
-              onClick={handleClick}
-            >
-              <Icon name="plus square outline" />
-              Set the Pedal dimensions
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 0}>
+        <Menu icon vertical borderless inverted>
+          <Popup
+            inverted
+            basic
+            on="click"
+            offset="50px, -50px"
+            style={{ marginLeft: "7px" }}
+            trigger={
+              <Menu.Item
+                name="select pedal"
+                color="blue"
+                active={activeItem === "select pedal"}
+                onClick={() =>
+                  activeItem !== "select pedal"
+                    ? setActiveItem("select pedal")
+                    : setActiveItem("")
+                }
+              >
+                <Icon name="folder" />
+              </Menu.Item>
+            }
+            content={<AvailablePedals pedals={pedals} dispatch={dispatch} />}
+          />
+          <Popup
+            inverted
+            basic
+            on="click"
+            offset="50px, -50px"
+            style={{ marginLeft: "7px" }}
+            trigger={
+              <Menu.Item
+                name="pedalform"
+                color="blue"
+                active={activeItem === "pedalform"}
+                onClick={() =>
+                  activeItem !== "pedalform"
+                    ? setActiveItem("pedalform")
+                    : setActiveItem("")
+                }
+              >
+                <Icon name="plus square outline" />
+              </Menu.Item>
+            }
+            content={
               <PedalForm
                 id={id}
                 width={width}
@@ -86,44 +102,114 @@ const Builder = ({ pedals }) => {
                 color={color}
                 dispatch={dispatch}
               />
-            </Accordion.Content>
+            }
+          />
 
-            <Accordion.Title
-              active={activeIndex === 1}
-              index={1}
-              onClick={handleClick}
-            >
-              <Icon name="circle notch" />
-              Add a Knob
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 1}>
-              <AddKnobForm dispatch={dispatch} />
-            </Accordion.Content>
+          <Popup
+            inverted
+            basic
+            on="click"
+            offset="50px, -50px"
+            style={{ marginLeft: "7px" }}
+            trigger={
+              <Menu.Item
+                name="add knob"
+                color="blue"
+                active={activeItem === "add knob"}
+                onClick={() =>
+                  activeItem !== "add knob"
+                    ? setActiveItem("add knob")
+                    : setActiveItem("")
+                }
+              >
+                <Icon name="circle notch" />
+              </Menu.Item>
+            }
+            content={<AddKnobForm dispatch={dispatch} />}
+          />
+          <Popup
+            inverted
+            basic
+            on="click"
+            offset="50px, -50px"
+            style={{ marginLeft: "7px" }}
+            trigger={
+              <Menu.Item
+                name="add switch"
+                color="blue"
+                active={activeItem === "add switch"}
+                onClick={() =>
+                  activeItem !== "add switch"
+                    ? setActiveItem("add switch")
+                    : setActiveItem("")
+                }
+              >
+                <Icon name="toggle on" />
+              </Menu.Item>
+            }
+            content={<AddSwitchForm dispatch={dispatch} />}
+          />
+          <Popup
+            inverted
+            basic
+            on="click"
+            offset="50px, -50px"
+            style={{ marginLeft: "7px" }}
+            trigger={
+              <Menu.Item
+                name="add indicator"
+                color="blue"
+                active={activeItem === "add indicator"}
+                onClick={() =>
+                  activeItem !== "add indicator"
+                    ? setActiveItem("add indicator")
+                    : setActiveItem("")
+                }
+              >
+                <Icon name="lightbulb" />
+              </Menu.Item>
+            }
+            content={<AddIndicatorForm dispatch={dispatch} />}
+          />
+          <Menu.Item
+            name="drag knob"
+            active={activeItem === "drag knob"}
+            onClick={() => {
+              dispatch({
+                type: "DRAG_KNOB"
+              });
+              activeItem !== "drag knob"
+                ? setActiveItem("drag knob")
+                : setActiveItem("");
+            }}
+          >
+            <Icon
+              name={drag ? "hand rock" : "hand paper"}
+              className="icon-pointer"
+              color={drag ? "orange" : "white"}
+            />
+          </Menu.Item>
+          <Menu.Item
+            name="tap knob in"
+            active={activeItem === "tap knob in"}
+            onClick={() => {
+              dispatch({
+                type: "TAP_KNOB"
+              });
+              activeItem !== "tap knob in"
+                ? setActiveItem("tap knob in")
+                : setActiveItem("");
+            }}
+          >
+            <Icon
+              name={tapKnobsIn ? "add circle" : "times circle"}
+              className="icon-pointer"
+              color={tapKnobsIn ? "yellow" : "white"}
+            />
+          </Menu.Item>
+        </Menu>
 
-            <Accordion.Title
-              active={activeIndex === 2}
-              index={2}
-              onClick={handleClick}
-            >
-              <Icon name="toggle on" />
-              Add a Switch
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 2}>
-              <AddSwitchForm dispatch={dispatch} />
-            </Accordion.Content>
-            <Accordion.Title
-              active={activeIndex === 3}
-              index={3}
-              onClick={handleClick}
-            >
-              <Icon name="toggle on" />
-              Add an Indicator
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 3}>
-              <AddIndicatorForm dispatch={dispatch} />
-            </Accordion.Content>
-          </Accordion>
-        </DivForm>
+        <Scaler scale={scale} dispatch={dispatch} />
         <DivDetails>
           <ComponentInfo
             knobs={knobs}
