1e7c319d7d7fcfd620b70d83e655795cbe3f8663
Eric Dulcet <etdulcet@gmail.com>
2019-11-02 21:26:03 -0400

    Move to using apollo-server-express

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: "patches-server",
-      url: "http://localhost:4000",
+      url: "http://localhost:4000/graphql",
       localSchemaFile: '/Users/ericdulcet/Code/patches/patches-server/src/schema.1.graphql',
     }
   }
