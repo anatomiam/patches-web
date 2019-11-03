5f81567b3bbc07489e15767ecd1b304942167de8
Eric Dulcet <etdulcet@gmail.com>
2019-11-03 10:27:34 -0500

    Create SignUpForm component

---
+++ src/components/Forms/General/SignUpForm.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Button, Input, Form } from "semantic-ui-react";
+import { ValidationErrors } from "../Shared/ValidationErrors";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { isEmpty } from "lodash";
+
+const SignUpSchema = Yup.object().shape({
+  username: Yup.string()
+    .max(200, "Username must be 200 characters or less")
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email addresss`")
+    .required("Required"),
+  password: Yup.string()
+    .max(200, "Password must be 200 characters or less")
+    .required("Required")
+});
+
+export const SignUpForm = React.memo(() => {
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={{
+        username: "",
+        email: "",
+        password: ""
+      }}
+      validationSchema={SignUpSchema}
+      validateOnChange={false}
+      validateOnBlur={false}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setSubmitting(false);
+      }}
+    >
+      {({
+        dirty,
+        values,
+        errors,
+        handleChange,
+        handleReset,
+        isSubmitting,
+        handleSubmit
+      }) => (
+        <Form
+          onSubmit={event => {
+            event.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <Form.Field>
+            <Input
+              id="username"
+              label="Username"
+              placeholder="Set Username"
+              name="username"
+              type="text"
+              onChange={handleChange}
+              value={values.username}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="email"
+              label="Email"
+              placeholder="Set Email"
+              name="email"
+              type="text"
+              onChange={handleChange}
+              value={values.email}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              id="password"
+              label="Password"
+              placeholder="Set Password"
+              name="password"
+              type="password"
+              onChange={handleChange}
+              value={values.password}
+            />
+          </Form.Field>
+          <Button type="submit" disabled={isSubmitting}>
+            Sign Up
+          </Button>
+          <Button type="button" disabled={!dirty} onClick={handleReset}>
+            Reset
+          </Button>
+          {!isEmpty(errors) ? <ValidationErrors errors={errors} /> : null}
+        </Form>
+      )}
+    </Formik>
+  );
+});
--- src/components/Modes/General/Landing.js
+++ src/components/Modes/General/Landing.js
@@ -1,66 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "../../../index.css";
-import styled from "styled-components";
-import { motion } from "framer-motion";
-import { Menu, Icon, Popup } from "semantic-ui-react";
-import { PedalForm } from "../../Forms/Builder/PedalForm";
-
-const DIV = styled(motion.div)`
-  padding: 5px;
-`;
+import { SignUpForm } from "../../Forms/General/SignUpForm";
 
 const Landing = () => {
-  const [activeItem, setActiveItem] = useState("");
-  return (
-    <DIV>
-      <Menu icon vertical borderless pointing inverted>
-        <Popup
-          inverted
-          basic
-          on="click"
-          offset="50px, -50px"
-          style={{ marginLeft: "7px" }}
-          trigger={
-            <Menu.Item
-              name="gamepad"
-              active={activeItem === "gamepad"}
-              onClick={() =>
-                activeItem !== "gamepad"
-                  ? setActiveItem("gamepad")
-                  : setActiveItem("")
-              }
-            >
-              <Icon name="gamepad" />
-            </Menu.Item>
-          }
-          content={<PedalForm />}
-        />
-
-        <Menu.Item
-          name="video camera"
-          active={activeItem === "video camera"}
-          onClick={() =>
-            activeItem !== "video camera"
-              ? setActiveItem("video camera")
-              : setActiveItem("")
-          }
-        >
-          <Icon name="video camera" />
-        </Menu.Item>
-        <Menu.Item
-          name="video play"
-          active={activeItem === "video play"}
-          onClick={() =>
-            activeItem !== "video play"
-              ? setActiveItem("video play")
-              : setActiveItem("")
-          }
-        >
-          <Icon name="video play" />
-        </Menu.Item>
-      </Menu>
-    </DIV>
-  );
+  return <SignUpForm />;
 };
 
 export default Landing;
