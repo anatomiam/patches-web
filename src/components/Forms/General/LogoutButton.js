ddaa1c73f159d5a44367fb1d9b5e6e4f09b5e983
Eric Dulcet <etdulcet@gmail.com>
2019-11-04 08:10:16 -0500

    Add LogoutButton component

---
+++ src/components/Forms/General/LogoutButton.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+import { setAccessToken } from "../../../state/Auth";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+import { withRouter } from "react-router";
+
+const LOGOUT = gql`
+  mutation Logout {
+    logout
+  }
+`;
+
+const LogoutButton = React.memo(({ history }) => {
+  const [logout, { client }] = useMutation(LOGOUT);
+  return (
+    <Button
+      content="Logout"
+      size="mini"
+      className="icon-pointer"
+      color="grey"
+      fluid
+      onClick={async event => {
+        event.preventDefault();
+        console.log("logging out");
+        await logout();
+        setAccessToken("");
+        console.log(localStorage);
+        // await client.resetStore();
+        // history.push("/");
+      }}
+    />
+  );
+});
+
+export const LogoutButtonWithRouter = withRouter(LogoutButton);
--- src/components/Modes/General/Login.js
+++ src/components/Modes/General/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { SignUpFormWithRouter } from "../../Forms/General/SignUpForm";
 import { LoginFormWithRouter } from "../../Forms/General/LoginForm";
+import { LogoutButtonWithRouter } from "../../Forms/General/LogoutButton";
 
 const Landing = () => {
   return (
     <>
       <SignUpFormWithRouter />;
       <LoginFormWithRouter />;
+      <LogoutButtonWithRouter />;
     </>
   );
 };
